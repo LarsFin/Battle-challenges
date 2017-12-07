b08e6e7dbd74f93d4147b12251ca6a3160bf2959
Léa R <lea.rosamond@gmail.com>
2017-12-07 11:09:49 +0000

    Refactoring the test with a separated method

--- spec/features/testing_infrastructure_spec.rb
+++ spec/features/testing_infrastructure_spec.rb
@@ -2,19 +2,15 @@
 feature 'Testing infrastructure' do
 
   scenario 'Will use a form to ask for a player name and display on screen' do
-    visit('/')
-    fill_in :player_1_name, with: "Jeff"
-    fill_in :player_2_name, with: "Jim"
-    click_button "Submit"
+    sign_in_and_play
     expect(page).to have_content "Player 1 Name: Jeff!" and have_content "Player 2 Name: Jim!" and have_content "Let's battle!!!"
   end
 
-  scenario 'see Player 2 Hit Points' do
-  visit('/')
-  fill_in :player_1_name, with: "Jeff"
-  fill_in :player_2_name, with: "Jim"
-  click_button "Submit"
+  scenario 'See Player 2 Hit Points' do
+    sign_in_and_play
   expect(page).to have_content "Jim Hit Points "
   end
+
+
 
 end
