93cb8df806a53f837e93586fa20c437b69e3cfcd
Lars <larsfinlay@googlemail.com>
2017-12-08 11:13:54 +0000

    Writing tests for multiplayer

--- spec/features/attack_spec.rb
+++ spec/features/attack_spec.rb
@@ -6,10 +6,16 @@ feature 'Attacking' do
     expect(page).to have_content "Jeff attacked Jim!"
   end
 
+  scenario 'Attacking Player 1 and getting confirmation' do
+    sign_in_and_play
+    attack_and_continue
+    click_button "Attack"
+    expect(page).to have_content "Jim attacked Jeff!"
+  end
+
   scenario "Deduct Player 2 's HP by 10" do
     sign_in_and_play
-    click_button "Attack"
-    click_button("Ok")
+    attack_and_continue
     expect(page).to have_content "Jim's HP = #{(Player::HP) - 10}"
   end
 end
--- spec/features/testing_infrastructure_spec.rb
+++ spec/features/testing_infrastructure_spec.rb
@@ -6,20 +6,19 @@ feature 'Testing infrastructure' do
     expect(page).to have_content "Player 1 Name: Jeff!" and have_content "Player 2 Name: Jim!" and have_content "Let's battle!!!"
   end
 
-  scenario 'See Player 2 Hit Points' do
+  scenario 'See Player 1 & Player 2 Hit Points' do
     sign_in_and_play
-    expect(page).to have_content "Jim's HP"
+    expect(page).to have_content "Jim's HP" and have_content "Jeff's HP"
   end
 
-  scenario 'Displays who played the turn' do
+  scenario 'Display current players turn' do
     sign_in_and_play
     expect(page).to have_content "Jeff's turn"
   end
 
   scenario 'Should switch turn when hitting ok after attack' do
     sign_in_and_play
-    click_button "Attack"
-    click_button("Ok")
+    attack_and_continue
     expect(page).to_not have_content "Jeff's turn"
     expect(page).to have_content "Jim's turn"
   end
